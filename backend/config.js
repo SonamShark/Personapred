6451567e04b80eb27496e0861dcad814b2f2e741
SonamShark <105120899+SonamShark@users.noreply.github.com>
2024-04-15 10:17:26 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,2 @@
 export const PORT = 5555;
-export const url = `mongodb+srv://sonam:s123@cluster0.7sylktc.mongodb.net`;
+export const url = `mongodb+srv://sonam:<password>@cluster0.7sylktc.mongodb.net`;
